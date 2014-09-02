8c16e2861811f80f96cdc1765b34c8d1e2b445a2
Miki Tebeka <miki.tebeka@gmail.com>
2014-09-02 09:02:47 +0300

    Version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if version_info[:2] < (2, 7):
 
 setup(
     name='whoops',
-    version='0.2.1',
+    version='0.2.2',
     description='WebHDFS client',
     long_description=open('README.rst').read(),
     author='Miki Tebeka',
